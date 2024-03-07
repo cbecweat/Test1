c721baf4b99b2caaf35da789a71edcc7311b31f8
cbecweat <157397251+cbecweat@users.noreply.github.com>
2024-03-07 13:40:25 +0000

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -1,54 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Registration</title>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-<?php
-require('db.php');
-// If form submitted, insert values into the database.
-if (isset($_REQUEST['username'])){
-        // removes backslashes
-	$username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
-	$username = mysqli_real_escape_string($con,$username); 
-	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($con,$email);
-	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($con,$password);
-	
-	// Generates a random salt
-	$salt = bin2hex(random_bytes(32));
-	$hashed_password = hash('sha256', $password . $salt);
-	
-	$trn_date = date("Y-m-d H:i:s");
-         $query = "INSERT into `users` (username, password, salt, email, trn_date)
-VALUES ('$username', '$hashed_password', '$salt', '$email', '$trn_date')";
-        $result = mysqli_query($con,$query);
-        if($result){
-            echo "<div class='form'>
+// Generate a random salt
+    $salt = bin2hex(random_bytes(16)); // 16 bytes (128 bits) salt
+
+    // Combine salt with the password
+    $password_with_salt = $password . $salt;
+
+    // Hash the password with salt
+    $hashed_password = hash('sha256', $password_with_salt);
+
+    $trn_date = date("Y-m-d H:i:s");
+    $query = "INSERT INTO `users` (username, password, salt, email, trn_date)
+            VALUES ('$username', '$hashed_password', '$salt', '$email', '$trn_date')";
+    $result = mysqli_query($con, $query);
+    if ($result) {
+        echo "<div class='form'>
 <h3>You are registered successfully.</h3>
 <br/>Click here to <a href='login.php'>Login</a></div>";
-
-	// Adds the php entitie that helps to prevent XSS attacks
-	htmlentities($username, ENT_QUOTES, 'UTF-8');
-	htmlentities($email, ENT_QUOTES, 'UTF-8');
-	htmlentities($password, ENT_QUOTES, 'UTF-8');
-		
-        }
-    }else{
-?>
-<div class="form">
-<h1>Registration</h1>
-<form name="registration" action="" method="post">
-<input type="text" name="username" placeholder="Username" required />
-<input type="email" name="email" placeholder="Email" required />
-<input type="password" name="password" placeholder="Password" required />
-<input type="submit" name="submit" value="Register" />
-</form>
-</div>
-<?php } ?>
-</body>
-</html>
